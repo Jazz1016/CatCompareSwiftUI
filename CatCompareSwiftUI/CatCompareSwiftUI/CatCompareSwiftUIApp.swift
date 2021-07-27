b6e27b7ef33c35572b3b6db0c1f201c7584d7930
James L <59751464+Jazz1016@users.noreply.github.com>
2021-07-27 14:29:22 -0600

    first commit

---
+++ CatCompareSwiftUI/CatCompareSwiftUI/CatCompareSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatCompareSwiftUIApp.swift
+//  CatCompareSwiftUI
+//
+//  Created by James Lea on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct CatCompareSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
