3017ec01f63dffd559152b02957da1dce7a07fc8
James L <59751464+Jazz1016@users.noreply.github.com>
2021-07-27 14:53:41 -0600

    added model and network call

---
+++ CatCompareSwiftUI/CatCompareSwiftUI/Helper & Extension/CatError.swift
@@ -0,0 +1,27 @@
+//
+//  CatError.swift
+//  CatCompareSwiftUI
+//
+//  Created by James Lea on 7/27/21.
+//
+
+import Foundation
+
+enum CatError: LocalizedError {
+    case invalidURL
+    case thrownError(Error)
+    case noData
+    case unableToDecode
+    var errorDescription: String? {
+        switch self {
+        case .thrownError(let error):
+            return "Error: \(error.localizedDescription) -> \(error)"
+        case .invalidURL:
+            return "Unable to reach the server."
+        case .noData:
+            return "The server responded with no data."
+        case .unableToDecode:
+            return "I'm a scrub"
+        }
+    }
+}
